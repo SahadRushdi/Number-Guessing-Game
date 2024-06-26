914656ec9982168020805fed9f9b5a8ad96ccf27
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-06-26 14:57:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// console.log("Hello");
+
+//Before method
+// Customer() {
+
+// }
+
+// class Customer {
+//     name;
+//     age;
+//     address;
+
+//     //Above method using CONSTRUCTORS
+//     constructor(name, age, address) {
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+
+//     setName(name) {
+//         this.name = name;
+//     }
+//     getName() {
+//         return this.name;
+//     }
+
+//     setAge(age) {
+//         this.age = age;
+//     }
+//     getAge() {
+//         return this.age;
+//     }
+// }
+
+// let customer01 = new Customer("Saad", 18, "Colombo");
+
+// console.log(customer01.getName());
+// console.log(customer01.getAge());
+
+
+let randomNumber = Math.floor((Math.random()*10)+1);
+console.log(randomNumber);
